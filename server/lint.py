77474e94e3e8e382810f0bf03711adbc860ab344
Richard Stoffels <richard@sinzig-stoffels.de>
2024-03-07 12:36:45 +0100

    Initialize server project with requirements and linting

---
+++ server/lint.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+
+def call(args):
+    status = os.system(" ".join(args))
+    if status != 0:
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    fix = len(sys.argv) > 1 and sys.argv[1] == "--fix"
+    extra_args = [] if fix else ["--check"]
+    call(["isort", "."] + extra_args)
+    call(["black", "-l", "80", "-C", "."] + extra_args)
