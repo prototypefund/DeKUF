dfce90a8154d0a4a024e844808a15326cebff576
Richard Stoffels <richard@sinzig-stoffels.de>
2024-03-07 18:20:08 +0100

    Add Admin view for adding survey and queries

--- server/core/admin.py
+++ server/core/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Commissioner, Query, Survey
+
+
+@admin.register(Commissioner)
+class CommissionerAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+class QueryInline(admin.TabularInline):
+    model = Query
+    extra = 1
+
+
+@admin.register(Survey)
+class SurveyAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    inlines = [QueryInline]
+    search_fields = ("commissioners__name",)
+    filter_horizontal = ("commissioners",)
--- server/core/models.py
+++ server/core/models.py
@@ -4,9 +4,16 @@ from django.db import models
 class Commissioner(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
 
 class Survey(models.Model):
+    name = models.CharField(max_length=200)
     commissioners = models.ManyToManyField(Commissioner)
+
+    def __str__(self):
+        return self.name
 
 
 class Query(models.Model):
@@ -14,6 +21,9 @@ class Query(models.Model):
         Survey, on_delete=models.CASCADE, related_name="queries"
     )
     data_key = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Query on {self.data_key}"
 
 
 class SurveyResponse(models.Model):
