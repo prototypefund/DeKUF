f98f7f0b7f325dc0fb6becb6af5040e65ef74294
Richard Stoffels <richard@sinzig-stoffels.de>
2024-03-07 13:59:53 +0100

    Server: Add Survey and Query

--- server/core/models.py
+++ server/core/models.py
@@ -5,3 +5,14 @@ from django.db import models
 
 class JsonData(models.Model):
     data: Dict[str, Any] = models.JSONField()
+
+
+class Survey(models.Model):
+    issuer: str = models.CharField(max_length=100)
+
+
+class Query(models.Model):
+    survey: Survey = models.ForeignKey(
+        Survey, on_delete=models.CASCADE, related_name="queries"
+    )
+    dataKey: str = models.CharField()
