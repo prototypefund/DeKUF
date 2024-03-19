8e08d4f86b5a83c4a9430c61518492823f7a03b4
Felix H. Dahlke <fhd@uberco.de>
2024-03-19 17:05:47 +0100

    Add missing virtual destructor

--- client/src/storage.hpp
+++ client/src/storage.hpp
@@ -4,6 +4,7 @@
 
 class Storage {
 public:
+    virtual ~Storage() {};
     virtual QList<QString> listDataPoints(const QString& key) = 0;
     virtual void addDataPoint(const QString& key, const QString& value) = 0;
 };
