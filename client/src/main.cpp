00fdcf0a2dfdc6228c7350ea99774852ec9ec635
Felix H. Dahlke <fhd@uberco.de>
2024-03-19 15:09:06 +0100

    Turn the client into a daemon
    Resolves #5.

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,10 +1,22 @@
 #include "client.hpp"
 
+const int initialDelay = 1000;
+const int interval = 60000;
+
 int main(int argc, char* argv[])
 {
     QCoreApplication app(argc, argv);
     auto client = new Client(&app);
-    QObject::connect(client, SIGNAL(finished()), &app, SLOT(quit()));
-    QTimer::singleShot(1000, client, SLOT(run()));
+    auto timer = new QTimer(&app);
+
+    QObject::connect(timer, &QTimer::timeout, &app, [&]() {
+        timer->stop();
+        client->run();
+    });
+
+    QObject::connect(
+        client, &Client::finished, &app, [&]() { timer->start(interval); });
+
+    timer->start(initialDelay);
     return app.exec();
 }
