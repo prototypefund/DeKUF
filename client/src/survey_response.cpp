3e43041f8b2f1c9a52a164c0997b991cc2ccb00d
Felix H. Dahlke <fhd@uberco.de>
2024-04-03 11:42:50 +0200

    Add implementation file for SurveyResponse
    Resolves #18.

---
+++ client/src/survey_response.cpp
@@ -0,0 +1,64 @@
+#include "commissioner.hpp"
+#include "storage.hpp"
+#include "survey.hpp"
+
+#include "survey_response.hpp"
+
+QueryResponse::QueryResponse(const QString& dataKey, const QString& data)
+    : dataKey(dataKey)
+    , data(data)
+{
+}
+
+QSharedPointer<SurveyResponse> SurveyResponse::create(
+    QSharedPointer<Survey> survey, Storage& storage)
+{
+    // Only KDE allowed as commissioner
+    QString kdeName("KDE");
+    if (!std::any_of(survey->commissioners.begin(), survey->commissioners.end(),
+            [&](const QSharedPointer<Commissioner>& commissioner) {
+                return commissioner->name == kdeName;
+            }))
+        return QSharedPointer<SurveyResponse>();
+
+    auto surveyResponse = QSharedPointer<SurveyResponse>::create();
+    surveyResponse->commissioners.append(
+        QSharedPointer<Commissioner>::create(kdeName));
+
+    for (auto query : survey->queries) {
+        auto dataPoints = storage.listDataPoints(query->dataKey);
+        if (dataPoints.count() == 0)
+            continue;
+        surveyResponse->queryResponses.append(
+            QSharedPointer<QueryResponse>::create(
+                query->dataKey, dataPoints.first()));
+    }
+    return surveyResponse;
+}
+
+QByteArray SurveyResponse::toJsonByteArray()
+{
+    QJsonObject surveyJsonResponse;
+    QJsonArray queryJsonResponses;
+    QJsonArray commissionersJson;
+
+    for (auto queryResponse : queryResponses) {
+        QJsonObject queryJsonResponse;
+        queryJsonResponse["dataKey"] = queryResponse->dataKey;
+        queryJsonResponse["data"] = queryResponse->data;
+        queryJsonResponses.push_back(queryJsonResponse);
+    }
+
+    for (auto commissioner : commissioners) {
+        QJsonObject commissionerJson;
+        commissionerJson["name"] = commissioner->name;
+        commissionersJson.push_back(commissionerJson);
+    }
+
+    surveyJsonResponse["queryResponses"] = queryJsonResponses;
+    surveyJsonResponse["commissioners"] = commissionersJson;
+
+    QJsonDocument root;
+    root.setObject(surveyJsonResponse);
+    return root.toJson();
+}
--- client/src/survey_response.hpp
+++ client/src/survey_response.hpp
@@ -1,19 +1,17 @@
 #pragma once
 
-#include "commissioner.hpp"
-#include "storage.hpp"
-#include "survey.hpp"
 #include <QtCore>
+
+class Commisioner;
+class Survey;
+class SurveyResponse;
 
 class QueryResponse {
 public:
-    QueryResponse(const QString& dataKey, const QString& data)
-        : dataKey(dataKey)
-        , data(data)
-    {
-    }
     const QString dataKey;
     const QString data;
+
+    QueryResponse(const QString& dataKey, const QString& data);
 };
 
 class SurveyResponse {
@@ -22,56 +20,7 @@ public:
     QList<QSharedPointer<QueryResponse>> queryResponses;
 
     static QSharedPointer<SurveyResponse> create(
-        QSharedPointer<Survey> survey, Storage& storage)
-    {
-        // Only KDE allowed as commissioner
-        QString kdeName("KDE");
-        if (!std::any_of(survey->commissioners.begin(),
-                survey->commissioners.end(),
-                [&](const QSharedPointer<Commissioner>& commissioner) {
-                    return commissioner->name == kdeName;
-                }))
-            return QSharedPointer<SurveyResponse>();
+        QSharedPointer<Survey> survey, Storage& storage);
 
-        auto surveyResponse = QSharedPointer<SurveyResponse>::create();
-        surveyResponse->commissioners.append(
-            QSharedPointer<Commissioner>::create(kdeName));
-
-        for (auto query : survey->queries) {
-            auto dataPoints = storage.listDataPoints(query->dataKey);
-            if (dataPoints.count() == 0)
-                continue;
-            surveyResponse->queryResponses.append(
-                QSharedPointer<QueryResponse>::create(
-                    query->dataKey, dataPoints.first()));
-        }
-        return surveyResponse;
-    }
-
-    QByteArray toJsonByteArray()
-    {
-        QJsonObject surveyJsonResponse;
-        QJsonArray queryJsonResponses;
-        QJsonArray commissionersJson;
-
-        for (auto queryResponse : queryResponses) {
-            QJsonObject queryJsonResponse;
-            queryJsonResponse["dataKey"] = queryResponse->dataKey;
-            queryJsonResponse["data"] = queryResponse->data;
-            queryJsonResponses.push_back(queryJsonResponse);
-        }
-
-        for (auto commissioner : commissioners) {
-            QJsonObject commissionerJson;
-            commissionerJson["name"] = commissioner->name;
-            commissionersJson.push_back(commissionerJson);
-        }
-
-        surveyJsonResponse["queryResponses"] = queryJsonResponses;
-        surveyJsonResponse["commissioners"] = commissionersJson;
-
-        QJsonDocument root;
-        root.setObject(surveyJsonResponse);
-        return root.toJson();
-    }
+    QByteArray toJsonByteArray();
 };
