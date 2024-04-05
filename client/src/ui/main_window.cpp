50de86e436b8182dbfa108ff28753187f3000346
Felix H. Dahlke <fhd@uberco.de>
2024-04-05 21:05:16 +0200

    Show survey responses in the UI in a crude fashion
    Pretty temporary state of things, but it's visible progress at least. Two issues
    that need to be addressed:
    
    1. Show the responses in a more meaningful way than just JSON serialized.
    2. Move *Storage and SurveyResponse to a new "core" library, as the TODO
       comments say.
    
    Relates to #14.

--- client/src/ui/main_window.cpp
+++ client/src/ui/main_window.cpp
@@ -1,10 +1,42 @@
 #include "main_window.hpp"
 #include "ui_main_window.h"
 
+// TODO: Move *Storage and SurveyResponse to a dedicated "core" library instead.
+#include "../daemon/sqlite_storage.hpp"
+#include "../daemon/survey_response.hpp"
+
+namespace {
+void setupTableWidget(QTableWidget& tableWidget)
+{
+    // TODO: Probably better to set these in the designer.
+    tableWidget.setColumnCount(1);
+    tableWidget.horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+    tableWidget.setEditTriggers(QAbstractItemView::NoEditTriggers);
+
+    // For a first PoC, we're reading data directly from the daemon's SQLite
+    // database, but we plan to switch to IPC:
+    // https://gitlab.com/privact/dekuf/-/issues/14
+    SqliteStorage storage;
+    int row = 0;
+    for (const auto& response : storage.listSurveyResponses()) {
+        tableWidget.insertRow(row);
+        // TODO: Show something more user-friendly than the serialized
+        //       response.
+        QString text(response->toJsonByteArray());
+        text.replace("\n", " ");
+        auto item = new QTableWidgetItem; // NOLINT
+        item->setText(text);
+        tableWidget.setItem(row, 0, item);
+        row++;
+    }
+}
+}
+
 MainWindow::MainWindow(QWidget* parent)
     : ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    setupTableWidget(*ui->tableWidget);
 }
 
 MainWindow::~MainWindow() { delete ui; }
