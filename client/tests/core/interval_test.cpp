ba8757004eb1cffb0a340016c1b5fcfeba97e624
Felix H. Dahlke <fhd@uberco.de>
2024-05-14 13:34:54 +0200

    Add tests for Interval

---
+++ client/tests/core/interval_test.cpp
@@ -0,0 +1,64 @@
+#include <QTest>
+
+#include <core/interval.hpp>
+#include <qglobal.h>
+#include <qtestcase.h>
+
+#include "interval_test.hpp"
+
+void IntervalTest::testOpen()
+{
+    Interval interval("[0, 2]");
+    QCOMPARE(interval.isInInterval(-1), false);
+    QCOMPARE(interval.isInInterval(0), true);
+    QCOMPARE(interval.isInInterval(1), true);
+    QCOMPARE(interval.isInInterval(2), true);
+    QCOMPARE(interval.isInInterval(3), false);
+}
+
+void IntervalTest::testClosed()
+{
+    Interval interval("(0, 2)");
+    QCOMPARE(interval.isInInterval(-1), false);
+    QCOMPARE(interval.isInInterval(0), false);
+    QCOMPARE(interval.isInInterval(1), true);
+    QCOMPARE(interval.isInInterval(2), false);
+    QCOMPARE(interval.isInInterval(3), false);
+}
+
+void IntervalTest::testMixed()
+{
+    Interval first("[0, 2)");
+    QCOMPARE(first.isInInterval(-1), false);
+    QCOMPARE(first.isInInterval(0), true);
+    QCOMPARE(first.isInInterval(1), true);
+    QCOMPARE(first.isInInterval(2), false);
+    QCOMPARE(first.isInInterval(3), false);
+
+    Interval second("(0, 2]");
+    QCOMPARE(second.isInInterval(-1), false);
+    QCOMPARE(second.isInInterval(0), false);
+    QCOMPARE(second.isInInterval(1), true);
+    QCOMPARE(second.isInInterval(2), true);
+    QCOMPARE(second.isInInterval(3), false);
+}
+
+void IntervalTest::testInvalid()
+{
+    (Interval("[1, 2]"));
+    (Interval("(1, 2)"));
+    (Interval("[1, 2)"));
+    (Interval("(1, 2]"));
+    (Interval("[1,2]"));
+
+    QVERIFY_EXCEPTION_THROWN((Interval("")), std::invalid_argument);
+    QVERIFY_EXCEPTION_THROWN((Interval("1")), std::invalid_argument);
+    QVERIFY_EXCEPTION_THROWN((Interval("1, 2")), std::invalid_argument);
+    QVERIFY_EXCEPTION_THROWN((Interval("[1 2]")), std::invalid_argument);
+    QVERIFY_EXCEPTION_THROWN((Interval("[1, 2")), std::invalid_argument);
+    QVERIFY_EXCEPTION_THROWN((Interval("1, 2]")), std::invalid_argument);
+    QVERIFY_EXCEPTION_THROWN((Interval("[ 1, 2]")), std::invalid_argument);
+    QVERIFY_EXCEPTION_THROWN((Interval("[1, 2 ]")), std::invalid_argument);
+}
+
+QTEST_MAIN(IntervalTest)
---
+++ client/tests/core/interval_test.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <QObject>
+
+class IntervalTest : public QObject {
+    Q_OBJECT
+
+private slots:
+    void testOpen();
+    void testClosed();
+    void testMixed();
+    void testInvalid();
+};
