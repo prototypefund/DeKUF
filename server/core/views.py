11025b6f646fbcf4eb0222a164d453b102b55f7d
Richard Stoffels <richard@sinzig-stoffels.de>
2024-06-04 14:29:46 +0200

    These different formatters make me go mad
    Working on #9

--- server/core/views.py
+++ server/core/views.py
@@ -52,7 +52,9 @@ def signup_to_survey(request, survey_id):
         }
 
         group_ungrouped_signups(
-            ungrouped_signups=list(SurveySignup.objects.filter(group__isnull=True)),
+            ungrouped_signups=list(
+                SurveySignup.objects.filter(group__isnull=True)
+            ),
             survey=survey,
         )
 
