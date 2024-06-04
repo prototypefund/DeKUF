10d8e017674fc8ced23eb1ef6c9d151db53b3931
Richard Stoffels <richard@sinzig-stoffels.de>
2024-06-04 13:38:53 +0200

    Add tests for survey_signup post endpoint
    Working on #9

---
+++ server/core/tests/test_federated_aggregation_endpoints.py
@@ -0,0 +1,37 @@
+import uuid
+
+from django.test import TestCase
+from django.urls import reverse
+
+from core.models.commissioner import Commissioner
+from core.models.survey import Survey
+from core.models.survey_signup import SurveySignup
+
+
+class MyPostViewTest(TestCase):
+
+    def setUp(self):
+        commissioner = Commissioner.objects.create(name="TestCommissioner")
+        self.survey = Survey.objects.create(
+            name="TestSurvey", commissioner=commissioner
+        )
+
+    def test_post_endpoint_201_with_correct_survey(self):
+        url = reverse('survey-signup', args=[self.survey.id])
+
+        self.assertEqual(len(SurveySignup.objects.all()), 0)
+
+        response = self.client.post(url,
+                                    content_type='application/x-www-form-urlencoded')
+
+        self.assertEqual(len(SurveySignup.objects.all()), 1)
+
+        self.assertEqual(response.status_code, 201)
+
+    def test_post_endpoint_404_with_nonexistent_survey(self):
+        url = reverse('survey-signup', args=[uuid.uuid4()])
+
+        response = self.client.post(url,
+                                    content_type='application/x-www-form-urlencoded')
+
+        self.assertEqual(response.status_code, 404)
